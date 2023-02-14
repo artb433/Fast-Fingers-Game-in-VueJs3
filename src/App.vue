9a6f1a0ccc244e299e55df0b704bc1884d51f5b7
artb433 <tahiruabdulrahmanbaaba3@gmail.com>
2023-02-14 02:02:40 +0000

    Starter files 📂

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{title}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'ART.B  TECHNOLOGIES'
+    }
   }
+
+  
 }
 </script>
 
@@ -22,5 +27,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} 
+
+h1 {
+border-bottom: 1px solid #ddd;
+display: inline-block;
+padding-bottom: 10px;
 }
 </style>
